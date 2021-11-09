a1cc2fa1df3cc5135898a050aea177f1b702a3c2
jashmin patel <zasminpatel@gmail.com>
2021-11-09 12:33:33 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -30,15 +30,3 @@ const App = () => {
   );
 };
 export default App;
-
-// import "./styles.css";
-// import Button from '@mui/material/Button';
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <Button variant="contained">Hello World</Button>
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//     </div>
-//   );
-// }
